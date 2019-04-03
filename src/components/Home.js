99c0062318627910fdcaef58389c0fe9347bbd72
stephen112233 <applejuicekid@gmail.com>
2019-04-03 15:29:10 -0400

    fixes home  page

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import '../css/appcss.css'
 
 class Home extends Component {
-  render() {
+  render () {
     return (
       <div id="home">
         <img className="Khaled" src="https://media1.tenor.com/images/32e5dad36611aaa9835dbffa34901450/tenor.gif?itemid=5348416"></img>
